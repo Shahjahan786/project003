e0a8b33c07f5b8eb96a69840a9aea698c26022ae
shahjahansamon <samoon.shahjahan@gmail.com>
2020-12-22 14:27:31 +0500

    used useState

---
+++ src/MediaCard.js
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+function MediaCard(props){
+    let [count, setCount]  =useState(1)
+    return <div>
+        <h2>{props.title + " ( "+ count +" )" }</h2>
+        <p>{props.body}</p>
+        <img src= {props.imageUrl} width="200" height = "200"/>
+        <br/>
+        <button onClick={() =>  setCount(++count) }>Update</button>
+    </div>
+}
+
+export default MediaCard
